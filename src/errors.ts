f9ee22c6b900d444c4bc47d34d3ffb514626be0e
alpoi <al.pointer@outlook.com>
2023-08-07 19:00:21 +0100

    chore: decouple voice service from message service, use errors for control flow (to be propagated to the handler)

---
+++ src/errors.ts
@@ -0,0 +1,5 @@
+export class UserVoiceChannelNull extends Error {}
+export class UserVoiceChannelMismatch extends Error {}
+export class BigweldVoiceChannelNull extends Error {}
+export class BigweldAlreadyPresent extends Error {}
+export class BigweldElsewhere extends Error {}
--- src/services/voiceService.ts
+++ src/services/voiceService.ts
@@ -1,109 +1,75 @@
 import DiscordClient from "../client";
 import { ChatInputCommandInteraction, GuildMember } from "discord.js";
+import { VoiceConnection, VoiceConnectionStatus, entersState, joinVoiceChannel, getVoiceConnections } from "@discordjs/voice";
 import {
-    VoiceConnection,
-    VoiceConnectionStatus,
-    entersState,
-    joinVoiceChannel,
-    getVoiceConnections
-} from "@discordjs/voice";
+    BigweldAlreadyPresent, BigweldElsewhere,
+    BigweldVoiceChannelNull,
+    UserVoiceChannelMismatch,
+    UserVoiceChannelNull
+} from "../errors";
 
 export default class VoiceService {
     public client: DiscordClient;
-    public voiceConnection: VoiceConnection | undefined = undefined;
-    public channelId: string | undefined = undefined;
+    public voiceConnection?: VoiceConnection | null;
+    public channelId?: string | null;
 
     constructor(client: DiscordClient) {
         this.client = client;
     }
 
     private async setConnection(connection: VoiceConnection) : Promise<void> {
-        if (this.voiceConnection) {
-            await this.destroyConnection();
-        }
+        if (this.voiceConnection) await this.destroyConnection();
         this.voiceConnection = connection;
     }
 
     private async destroyConnection() : Promise<void> {
         this.voiceConnection?.destroy();
-        this.voiceConnection = undefined;
+        this.voiceConnection = null;
     }
 
     private async destroyAllConnections(group: string = "default") : Promise<void> {
         const connections: Map<string, VoiceConnection> | undefined = getVoiceConnections(group);
-        if (connections) {
-            connections.forEach((conn: VoiceConnection) => conn.destroy());
-        }
+        if (connections) connections.forEach((conn: VoiceConnection) => conn.destroy());
     }
 
-    public async userConnectedWithBigweld(interaction: ChatInputCommandInteraction) : Promise<boolean> {
+    public async ensureUserConnectedWithBigweld(interaction: ChatInputCommandInteraction) : Promise<void> {
         const member: GuildMember = interaction.member as GuildMember;
 
-        if (!member.voice.channelId) {
-            await this.client.messageService.userNotInAnyVoiceChannelMessage(interaction);
-        } else if (!this.channelId) {
-            await this.client.messageService.notInVoiceChannelMessage(interaction);
-        } else if (member.voice.channelId !== this.channelId) {
-            await this.client.messageService.userNotInGivenVoiceChannelMessage(interaction, this.channelId);
-        } else {
-            return true;
-        }
-
-        return false;
+        if (!member.voice.channelId) throw new UserVoiceChannelNull();
+        if (!this.channelId) throw new BigweldVoiceChannelNull();
+        if (member.voice.channelId !== this.channelId) throw new UserVoiceChannelMismatch();
     }
 
     async pausePlayback(interaction: ChatInputCommandInteraction) : Promise<void> {
-        if (!await this.userConnectedWithBigweld(interaction)) {
-            return;
-        }
-
+        await this.ensureUserConnectedWithBigweld(interaction)
         // TODO implement
-        await this.client.messageService.pausedPlaybackMessage(interaction);
-
     }
 
     async resumePlayback(interaction: ChatInputCommandInteraction) : Promise<void> {
-        if (!await this.userConnectedWithBigweld(interaction)) {
-            return;
-        }
-
+        await this.ensureUserConnectedWithBigweld(interaction)
         // TODO implement
-        await this.client.messageService.resumedPlaybackMessage(interaction);
     }
 
     async joinVoiceChannel(interaction: ChatInputCommandInteraction) : Promise<void> {
         const member: GuildMember = interaction.member as GuildMember;
 
-        if (!member.voice.channelId) {
-            await this.client.messageService.userNotInAnyVoiceChannelMessage(interaction);
-        } else if (this.voiceConnection && this.channelId === member.voice.channelId) {
-            await this.client.messageService.alreadyInVoiceChannelMessage(interaction);
-        } else if (this.voiceConnection) {
-            await this.client.messageService.inAnotherVoiceChannelMessage(interaction);
-        } else {
-            const connection: VoiceConnection = joinVoiceChannel({
-                channelId: member.voice.channelId,
-                guildId: interaction.guildId!,
-                adapterCreator: interaction.guild!.voiceAdapterCreator
-            });
-            this.channelId = member.voice.channelId;
-            await this.setConnection(connection);
-            await this.client.messageService.joinedVoiceChannelMessage(interaction);
-        }
+        if (!member.voice.channelId) throw new UserVoiceChannelNull();
+        if (this.channelId === member.voice.channelId) throw new BigweldAlreadyPresent();
+        if (this.channelId && this.channelId !== member.voice.channelId) throw new BigweldElsewhere();
+
+        const connection: VoiceConnection = joinVoiceChannel({
+            channelId: member.voice.channelId,
+            guildId: interaction.guildId!,
+            adapterCreator: interaction.guild!.voiceAdapterCreator
+        });
+
+        this.channelId = member.voice.channelId;
+        await this.setConnection(connection);
     }
 
     async leaveVoiceChannel(interaction: ChatInputCommandInteraction) : Promise<void> {
-        if (!await this.userConnectedWithBigweld(interaction)) {
-            return;
-        }
-
-        if (this.voiceConnection) {
-            await this.destroyConnection();
-            await this.client.messageService.leftVoiceChannelMessage(interaction);
-        } else {
-            await this.client.messageService.notInVoiceChannelMessage(interaction);
-        }
-
-        this.channelId = undefined;
+        await this.ensureUserConnectedWithBigweld(interaction);
+        if (!this.channelId) throw new BigweldVoiceChannelNull();
+        this.channelId = null;
     }
 }
